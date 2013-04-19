468c2b956c255917f957b6e8f8ee9e29020f9494
Julien 'Lta' BALLET <elthariel@gmail.com>
2013-04-19 21:07:27 +0200

    Initial commit

---
+++ include/mfx_mingw.h
@@ -0,0 +1,31 @@
+/*
+** mfx_mingw.h
+** Login : <lta@still>
+** Started on  Fri Apr 19 19:01:21 2013 Lta Akr
+** $Id$
+**
+** Author(s):
+**  - Julien 'Lta' BALLET <contact@lta.io>
+**
+** Copyright (C) 2013 Lta Akr
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef   	MFX_MINGW_H_
+# define   	MFX_MINGW_H_
+
+# include <_mingw.h>
+
+#endif 	    /* !MFX_MINGW_H_ */
